0a84fe348086d1d05c9ff785b4aa13ba36935648
nerius.perez <ntoirac@proveedor.teco.com.ar>
2020-06-23 10:39:39 -0300

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.port || 3000
 /* console.log(__dirname)
 console.log(__filename) */
 
@@ -101,6 +102,6 @@ app.get('*', (req, res) => {
 //http://app.com/help
 //http://app.com/about
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000!!!')
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}!!!´)
 })
